2657ac78c18f5cf856f8b7d817eba447590f90af
Pysavant <110984357+pysavantcodes@users.noreply.github.com>
2023-04-30 00:38:12 +0100

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,4 @@
-llpimport Nav from "@/components/Nav";
+import Nav from "@/components/Nav";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { motion } from "framer-motion";
